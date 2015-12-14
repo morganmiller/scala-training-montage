41daf23962055e52def0727993f019342d62dbad
Morgan Miller <m.miller722@gmail.com>
2015-12-13 17:40:46 -0700

    finish syntax introduction chapter 2

---
+++ programming-in-scala/chap-2/pa.scala
@@ -0,0 +1,6 @@
+/** concise, functional way to print each argument in console */
+args.foreach(arg => println(arg))
+
+/** This will also work, for a function literal that takes a single statement and single argument:
+args.foreach(println)
+*/
