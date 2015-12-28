5ae06d32279722cfa247c4764014d7edc75d26a8
Morgan Miller <m.miller722@gmail.com>
2015-12-27 21:55:31 -0700

    case statements for Day 1 Round 2

--- advent-of-code/not-quite-lisp/solution.scala
+++ advent-of-code/not-quite-lisp/solution.scala
@@ -2,13 +2,14 @@ import scala.io.Source
 
 val directions = Source.fromFile("not-quite-lisp/input.txt").getLines.mkString
 
+//What floor does Santa end up on? Round One
 def countFloors(s: String, upOrDown: Char) = {
   s.count(c => c == upOrDown)
 }
 
 println(countFloors(directions, '(') - countFloors(directions, ')')) //280
 
-//refactor this with a case statement bc scala cases are rad
+//When does he make his first trip to the basement? Round Two: if/else way
 def basementPosition(s: String, index: Int = 0, floor: Int = 0):Int = {
   if(floor == -1){
     index
@@ -19,4 +20,17 @@ def basementPosition(s: String, index: Int = 0, floor: Int = 0):Int = {
   }
 }
 
-println(basementPosition(directions))
+println(basementPosition(directions)) //1797
+
+//Round Two: the neat scala-esque pattern matching way
+def basementPositionTwo(s: String, index: Int = 0, floor: Int = 0):Int = floor match {
+  case -1 => index
+  case _ => s(index) match {
+    case '(' => basementPositionTwo(s, index+1, floor+1)
+    case ')' => basementPositionTwo(s, index+1, floor-1)
+  }
+}
+
+println(basementPositionTwo(directions)) //1797
+
+//Can Round Two be done with PartialFunction and orElse?
