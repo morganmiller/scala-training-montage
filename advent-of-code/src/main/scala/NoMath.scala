440ff6e2c1bca98026ae518c66691909a938261d
Morgan Miller <m.miller722@gmail.com>
2015-12-27 20:34:31 -0700

    finish round 2 of advent of code day 2 and refactor

--- advent-of-code/src/main/scala/NoMath.scala
+++ advent-of-code/src/main/scala/NoMath.scala
@@ -1,23 +1,51 @@
 import scala.io.Source
 
-class PaperCalculator(dimensionsFilename: String) {
-  def orderAmount = {
-    val surfaceAreas = getDimensions.map(dimensions => surfaceArea(dimensions) + extraPaper(dimensions))
-    surfaceAreas.reduce(_+_)
-  }
-
+class OrderCalculator(dimensionsFilename: String) {
   def getDimensions = {
     val roughDimensions = Source.fromFile(dimensionsFilename).getLines.toList
     roughDimensions.map(lwh => lwh.split('x').toList.map(x => x.toInt))
   }
 
-  def surfaceArea(dimensions: List[Int]) = {
+  def paperAmount = {
+    val paperAmounts = getDimensions.map(dimensions => new Present(dimensions).wrappingPaper)
+    paperAmounts.reduce(_+_).toString + " feet"
+  }
+
+  def ribbonAmount = {
+    val ribbonAmounts = getDimensions.map(dimensions => new Present(dimensions).ribbonLength)
+    ribbonAmounts.reduce(_+_).toString + " feet"
+  }
+}
+
+class Present(dimensions: List[Int]) {
+
+  def wrappingPaper = {
+    surfaceArea + extraPaper
+  }
+
+  def ribbonLength = {
+    ribbonForBow + ribbonToWrap
+  }
+
+  def surfaceArea = {
     val multipliers = dimensions.drop(1)++dimensions.take(1)
     val products = dimensions.zip(multipliers).map({case (x,y) => x*y})
     products.reduce(_+_) * 2
   }
 
-  def extraPaper(dimensions: List[Int]) = {
-    dimensions.sortWith(_>_).drop(1).reduce(_*_)
+  def smallestSides = {
+    dimensions.sortWith(_>_).drop(1)
+  }
+
+  def extraPaper = {
+    smallestSides.reduce(_*_)
+  }
+
+  def ribbonForBow = {
+    dimensions.reduce(_*_)
+  }
+
+  def ribbonToWrap = {
+    smallestSides.reduce(_+_) * 2
   }
 }
--- advent-of-code/src/test/scala/NoMathTest.scala
+++ advent-of-code/src/test/scala/NoMathTest.scala
@@ -1,28 +1,49 @@
 import org.scalatest._
 
 class NoMathSpecs extends FlatSpec with Matchers {
-  def calculator = new PaperCalculator("src/test/input/presentdimensions.txt")
+//OrderCalculator tests
+  def calculator = new OrderCalculator("src/test/input/presentdimensions.txt")
 
   it should "format the dimensions of many presents" in {
     val presentDimensions = calculator.getDimensions
     presentDimensions should be(List(List(1, 2, 3), List(2, 3, 4)))
-    /** When this was: (List(Array(1,2,3), Array(2,3,4))),
-    Failed: List(Array(1, 2, 3), Array(2, 3, 4)) was not equal to List(Array(1, 2, 3), Array(2, 3, 4))
-    Wtf? Array/List behavior different? */
   }
 
+  it should "calculate the total paper needed from a text file of present dimensions" in {
+    val allThePaper = calculator.paperAmount
+    allThePaper should be ("82 feet")
+  }
+
+  it should "calculate the total ribbon needed for all presents" in {
+    val allTheRibbon = calculator.ribbonAmount
+    allTheRibbon should be("46 feet")
+  }
+
+//Present tests
+  def present = new Present(List(2,3,4))
+
   it should "find the surface area of one present" in {
-    val result = calculator.surfaceArea(List(2,3,4))
+    val result = present.surfaceArea
     result should be (52)
   }
 
   it should "find the extra paper needed for one present" in {
-    val result = calculator.extraPaper(List(2,3,4))
+    val result = present.extraPaper
     result should be (6)
   }
 
-  it should "calculate the total paper needed from a text file of present dimensions" in {
-    val allThePaper = calculator.orderAmount
-    allThePaper should be (82)
+  it should "find the ribbon needed to wrap the smallest perimeter" in {
+    val result = present.ribbonToWrap
+    result should be (10)
+  }
+
+  it should "calculate its volume to find ribbon needed for bow" in {
+    val result = present.ribbonForBow
+    result should be (24)
+  }
+
+  it should "calculate the total amount of ribbon needed for wrapping" in {
+    val result = present.ribbonLength
+    result should be (34)
   }
 }
